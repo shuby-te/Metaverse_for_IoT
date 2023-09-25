bb2adadd47dafd166c8ed7ae195371891602a6cd
portalchu <56888404+portalchu@users.noreply.github.com>
2023-09-25 19:42:25 +0900

    20230925_hyperledger-test

---
+++ HyperledgerIndy/Issuer/src/main/java/com/example/Issuer/Main.java
@@ -0,0 +1,23 @@
+package com.example.Issuer;
+
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+@SpringBootApplication
+public class Main {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Main.class, args);
+	}
+
+	@Bean
+	public CommandLineRunner commandLineRunner() {
+		return args -> {
+			System.out.println("Hello World");
+			
+		};
+	}
+
+}
---
+++ HyperledgerIndy/Issuer/src/test/java/com/example/Issuer/IssuerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Issuer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IssuerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
